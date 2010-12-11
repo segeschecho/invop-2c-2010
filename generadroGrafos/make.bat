51291489b589469cd693e9c40724ac11c814b5e0
seges.ar <seges.ar@4090385e-e651-79d7-09dc-6407a40382af>
2010-12-11 01:38:51 +0000

    subo un batch para generar casos de test con diferentes densidades

---
+++ generadroGrafos/make.bat
@@ -0,0 +1,24 @@
+::con 70% de densidad
+generador.py 3 70 > test3-70.txt
+generador.py 4 70 > test4-70.txt
+generador.py 5 70 > test5-70.txt
+generador.py 6 70 > test6-70.txt
+generador.py 7 70 > test7-70.txt
+generador.py 8 70 > test8-70.txt
+generador.py 9 70 > test9-70.txt
+::con 80%
+generador.py 3 80 > test3-80.txt
+generador.py 4 80 > test4-80.txt
+generador.py 5 80 > test5-80.txt
+generador.py 6 80 > test6-80.txt
+generador.py 7 80 > test7-80.txt
+generador.py 8 80 > test8-80.txt
+generador.py 9 80 > test9-80.txt
+::con 90%
+generador.py 3 90 > test3-90.txt
+generador.py 4 90 > test4-90.txt
+generador.py 5 90 > test5-90.txt
+generador.py 6 90 > test6-90.txt
+generador.py 7 90 > test7-90.txt
+generador.py 8 90 > test8-90.txt
+generador.py 9 90 > test9-90.txt
