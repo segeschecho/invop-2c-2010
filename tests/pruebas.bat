37fcafb4d19f1cea77767e2eaf47430f8ae59574
seges.ar <seges.ar@4090385e-e651-79d7-09dc-6407a40382af>
2010-12-17 16:13:48 +0000

    Subo los resultados de los tests con la variacion de los parametros nuevos. Tardan mucho mas, en comparación con los otros tests

--- tests/pruebas.bat
+++ tests/pruebas.bat
@@ -3,51 +3,8 @@ set densidad=70
 set inicio=3
 set fin=13
 set paso=1
+set tipos=24
 
-::parametros tipo 1
-set tipo=1
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 2
-set tipo=2
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 3
-set tipo=3
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 4
-set tipo=4
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 5
-set tipo=5
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 6
-set tipo=6
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 7
-set tipo=7
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 8
-set tipo=8
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 9
-set tipo=9
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 10
-set tipo=10
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 11
-set tipo=11
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
-
-::parametros tipo 12
-set tipo=12
-for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%tipo%.exe test%%X-%densidad%.txt > resultados\tipo%tipo%\res-test%%X-%densidad%.txt
+for /l %%T in (1,1,%tipos%) do (
+for /l %%X in (%inicio%, %paso%, %fin%) do ..\src\tp%%T.exe test%%X-%densidad%.txt > resultados\tipo%%T\res-test%%X-%densidad%.txt
+)
