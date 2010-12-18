787ea7003d27cf1f52821a58d599c4392ea83c59
seges.ar <seges.ar@4090385e-e651-79d7-09dc-6407a40382af>
2010-12-18 14:37:02 +0000

    Subo los tests hasta grafos de 13 nodos, con y sin las heuristicas iniciales. Listas para graficar

--- trunk/generadroGrafos/make.bat
+++ trunk/generadroGrafos/make.bat
@@ -1,8 +1,8 @@
 @echo off
 ::con 70% de densidad
 set densidad=70
-set inicio=10
-set fin=15
+set inicio=50
+set fin=50
 set paso=1
 ::        start, step, end
 for /l %%X in (%inicio%, %paso%, %fin%) do generador.py %%X %densidad% > test%%X-%densidad%.txt
