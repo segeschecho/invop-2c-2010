8b2254d1731c935e1fae4697585e237c75c8dc97
seges.ar <seges.ar@4090385e-e651-79d7-09dc-6407a40382af>
2010-12-26 17:48:34 +0000

    agrego tests de 60 65 y 70 nodos, con 30, 40, 80 y 90 % de densidad para el ejercicio 4

--- generadroGrafos/make.bat
+++ generadroGrafos/make.bat
@@ -1,14 +1,17 @@
 @echo off
 ::con 70% de densidad
-set densidad=70
-set inicio=15
-set fin=50
+set densidad=80
+set inicio=60
+set fin=70
 set paso=5
 ::        start, step, end
 for /l %%X in (%inicio%, %paso%, %fin%) do generador.py %%X %densidad% > test%%X-%densidad%.txt
 ::con 80%
-set densidad=80
-for /l %%X in (%inicio%, %paso%, %fin%) do generador.py %%X %densidad% > test%%X-%densidad%.txt
-::con 90%
 set densidad=90
+for /l %%X in (%inicio%, %paso%, %fin%) do generador.py %%X %densidad% > test%%X-%densidad%.txt
+::con 30%
+set densidad=30
+for /l %%X in (%inicio%, %paso%, %fin%) do generador.py %%X %densidad% > test%%X-%densidad%.txt
+::con 40%
+set densidad=40
 for /l %%X in (%inicio%, %paso%, %fin%) do generador.py %%X %densidad% > test%%X-%densidad%.txt
