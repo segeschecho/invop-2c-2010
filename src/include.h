7d46f011063103ea9bede74f973afcccd1c4c7c5
hadeskleizer <hadeskleizer@4090385e-e651-79d7-09dc-6407a40382af>
2010-12-26 21:30:50 +0000

    hecha toda la explicacion hasta ejercicio 2

--- src/include.h
+++ src/include.h
@@ -1,7 +1,8 @@
 #ifndef __include_H__
 #define __include_H__
 
-#include "C:\ILOG\CPLEX_Studio_AcademicResearch122\cplex\include\ilcplex\cplex.h"
+#include "C:\Program Files\CPLEX_Studio_AcademicResearch122\cplex\include\ilcplex\cplex.h"
+#include <assert.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
